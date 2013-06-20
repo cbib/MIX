08e6ae59ac702b623f29ab4b5eeeb72db4bd65b8
massyah <massyah@gmail.com>
2013-06-20 18:15:26 +0200

    Finished GAGE-B assemblies downloaders

--- datasets/GAGE-B/get_gage_b_assemblies.sh
+++ datasets/GAGE-B/get_gage_b_assemblies.sh
@@ -1,4 +1,4 @@
-wget ccb.jhu.edu/gage_b/genomeAssemblies/A_hydrophila_HiSeq.tar.gz \
+wget -N ccb.jhu.edu/gage_b/genomeAssemblies/A_hydrophila_HiSeq.tar.gz \
 ccb.jhu.edu/gage_b/genomeAssemblies/B_cereus_HiSeq.tar.gz \
 ccb.jhu.edu/gage_b/genomeAssemblies/B_cereus_MiSeq.tar.gz \
 ccb.jhu.edu/gage_b/genomeAssemblies/B_fragilis_HiSeq.tar.gz \
@@ -10,3 +10,11 @@ ccb.jhu.edu/gage_b/genomeAssemblies/S_aureus_HiSeq.tar.gz \
 ccb.jhu.edu/gage_b/genomeAssemblies/V_cholerae_HiSeq.tar.gz \
 ccb.jhu.edu/gage_b/genomeAssemblies/V_cholerae_MiSeq.tar.gz \
 ccb.jhu.edu/gage_b/genomeAssemblies/X_axonopodis_HiSeq.tar.gz 
+for file in `ls *.tar.gz`
+do
+TARGET="`basename $file .tar.gz`"
+mkdir $TARGET 
+tar zxvf $file -C $TARGET
+
+done
+rm *.tar.gz
