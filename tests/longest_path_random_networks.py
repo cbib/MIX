def2831702c794d1068765403c8fd945e936bcd4
massyah <massyah@gmail.com>
2013-06-21 19:48:57 +0200

    Added random grid generation

--- tests/longest_path_random_networks.py
+++ tests/longest_path_random_networks.py
@@ -103,38 +103,75 @@ def bounded_bfs_paths(G,sources,max_depth=None):
 		except StopIteration:
 			stack.pop(0)
 	
-g= nx.grid_graph(dim=[12,12])
-# Build mapping to rename nodes
-mapping = {}
-for k in g.node.keys():
-	mapping[k]="_".join(map(str,k))
-g=nx.relabel_nodes(g,mapping)
 
-# Make a directed graph by selecting a random direction for each edge
-g_dir=nx.DiGraph()
-for e in g.edges():
-	src,tgt = e
-	if random.random()>=0.5:
-		g_dir.add_edge(src,tgt)
+def make_random_directed_grid(dim=[5,5]):
+	g= nx.grid_graph(dim=[5,5])
+	# Build mapping to rename nodes
+	mapping = {}
+	for k in g.node.keys():
+		mapping[k]="_".join(map(str,k))
+	g=nx.relabel_nodes(g,mapping)
+
+	# Make a directed graph by selecting a random direction for each edge
+	g_dir=nx.DiGraph()
+	for e in g.edges():
+		src,tgt = e
+		if random.random()>=0.5:
+			g_dir.add_edge(src,tgt)
+		else:
+			g_dir.add_edge(tgt,src)
+
+	# Randomly place an input node 
+	no_input = [k for k,v in g_dir.in_degree().items() if v==0] 
+	if len(no_input)>0:
+		for n in no_input:
+			g_dir.node[n]['In']=True
 	else:
-		g_dir.add_edge(tgt,src)
+		g_dir.node[random.choice(g_dir.node.keys())]['In']=True
 
-# Randomly place an input node 
-no_input = [k for k,v in g_dir.in_degree().items() if v==0] 
-if len(no_input)>0:
-	for n in no_input:
-		g_dir.node[n]['In']=True
-else:
-	g_dir.node[random.choice(g_dir.node.keys())]['In']=True
+	no_output =[k for k,v in g_dir.out_degree().items() if v==0] 
 
-no_output =[k for k,v in g_dir.out_degree().items() if v==0] 
+	if len(no_output)>0:
+		for n in no_output:
+			g_dir.node[n]['Out']=True
 
-if len(no_output)>0:
-	for n in no_output:
-		g_dir.node[n]['Out']=True
+	else:
+		g_dir.node[random.choice(g_dir.node.keys())]['Out']=True
 
-else:
-	g_dir.node[random.choice(g_dir.node.keys())]['Out']=True
+	# Add constant weights 
+	for src,tgt in g_dir.edges():
+		g_dir[src][tgt]['weight']=1
+
+
+
+	# Plant a longest path by selecting a path and increasing it's weight
+	# Select a long path, max 1000 iter 
+	implanted_weight=50
+	max_path=[]
+	for i in range(1,1000):
+		starting_point = random.choice(no_input)
+		candidate_path = []
+		for node in nxrand.random_walk(g_dir,starting_point):
+			if node in candidate_path:
+				break
+			candidate_path.append(node)
+		if len(candidate_path)>=len(max_path):
+			max_path=candidate_path
+	print "Target path will be",max_path,"of length",implanted_weight*(len(max_path)-1)
+	for i in range(0,len(max_path)-1):
+		src,tgt=max_path[i],max_path[i+1]
+		g_dir[src][tgt]['weight']=implanted_weight
+	best_length=implanted_weight*(len(max_path)-1)
+	# Indicate them also as input and output so that we can find them 
+	g_dir.node[max_path[0]]['In']=True
+	g_dir.node[max_path[-1]]['Out']=True
+	return g_dir,max_path,best_length
+
+
+g_dir,max_path,best_length=make_random_directed_grid()
+
+
+
 
 
 # Randomly place an output node 
@@ -154,36 +191,6 @@ else:
 
 # g_dir.node[1]['In']=True
 # g_dir.node[3]['Out']=True
-
-
-# Add constant weights 
-for src,tgt in g_dir.edges():
-	g_dir[src][tgt]['weight']=1
-
-
-
-# Plant a longest path by selecting a path and increasing it's weight
-# Select a long path, max 1000 iter 
-implanted_weight=5
-max_path=[]
-for i in range(1,1000):
-	starting_point = random.choice(no_input)
-	candidate_path = []
-	for node in nxrand.random_walk(g_dir,starting_point):
-		if node in candidate_path:
-			break
-		candidate_path.append(node)
-	if len(candidate_path)>=len(max_path):
-		max_path=candidate_path
-print "Target path will be",max_path,"of length",implanted_weight*(len(max_path)-1)
-for i in range(0,len(max_path)-1):
-	src,tgt=max_path[i],max_path[i+1]
-	g_dir[src][tgt]['weight']=implanted_weight
-best_length=implanted_weight*(len(max_path)-1)
-# Indicate them also as input and output so that we can find them 
-g_dir.node[max_path[0]]['In']=True
-g_dir.node[max_path[-1]]['Out']=True
-
 
 
 
