118ed1f3e1d1b8ac5373791c3c9e9dae5ea2d0b4
massyah <massyah@gmail.com>
2013-06-29 23:46:00 +0200

    Added Mycoplasmas downloader

--- datasets/Mycoplasmas/get_mycoplasmas.sh
+++ datasets/Mycoplasmas/get_mycoplasmas.sh
@@ -1,34 +1,34 @@
 wget -N \
-./MAUR/single-assemblies/MAUR_CLC.fasta \
-./MAUR/single-assemblies/MAUR_ABySS_27.fasta \
-./MAUR/single-assemblies/MAUR_MIRA.fasta \
-./MBOVb/single-assemblies/MBOVb_CLC.fasta \
-./MBOVb/single-assemblies/MBOVb_MIRA.fasta \
-./MBOVb/single-assemblies/MBOVb_ABySS_34.fasta \
-./MBVG/single-assemblies/MBVG_CLC.fasta \
-./MBVG/single-assemblies/MBVG_MIRA.fasta \
-./MBVG/single-assemblies/MBVG_ABySS_35.fasta \
-./MCCP/single-assemblies/MCCP_CLC.fasta \
-./MCCP/single-assemblies/MCCP_MIRA.fasta \
-./MCCP/single-assemblies/MCCP_ABySS_35.fasta \
-./MMC/single-assemblies/MMC_CLC.fasta \
-./MMC/single-assemblies/MMC_MIRA.fasta \
-./MMC/single-assemblies/MMC_ABySS_30.fasta \
-./MOVI/single-assemblies/MOVI_CLC.fasta \
-./MOVI/single-assemblies/MOVI_MIRA.fasta \
-./MOVI/single-assemblies/MOVI_ABySS_32.fasta \
-./MSCb/single-assemblies/MSCb_CLC.fasta \
-./MSCb/single-assemblies/MSCb_MIRA.fasta \
-./MSCb/single-assemblies/MSCb_ABySS_29.fasta \
-./MSCc/single-assemblies/MSCc_CLC.fasta \
-./MSCc/single-assemblies/MSCc_MIRA.fasta \
-./MSCc/single-assemblies/MSCc_ABySS_27.fasta \
-./MSCd/single-assemblies/MSCd_CLC.fasta \
-./MSCd/single-assemblies/MSCd_MIRA.fasta \
-./MSCd/single-assemblies/MSCd_ABySS_34.fasta \
-./MSCe/single-assemblies/MSCe_CLC.fasta \
-./MSCe/single-assemblies/MSCe_MIRA.fasta \
-./MSCe/single-assemblies/MSCe_ABySS_35.fasta 
+http://services.cbib.u-bordeaux2.fr/mix/MAUR/single-assemblies/MAUR_CLC.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MAUR/single-assemblies/MAUR_ABySS_27.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MAUR/single-assemblies/MAUR_MIRA.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MBOVb/single-assemblies/MBOVb_CLC.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MBOVb/single-assemblies/MBOVb_MIRA.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MBOVb/single-assemblies/MBOVb_ABySS_34.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MBVG/single-assemblies/MBVG_CLC.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MBVG/single-assemblies/MBVG_MIRA.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MBVG/single-assemblies/MBVG_ABySS_35.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MCCP/single-assemblies/MCCP_CLC.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MCCP/single-assemblies/MCCP_MIRA.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MCCP/single-assemblies/MCCP_ABySS_35.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MMC/single-assemblies/MMC_CLC.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MMC/single-assemblies/MMC_MIRA.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MMC/single-assemblies/MMC_ABySS_30.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MOVI/single-assemblies/MOVI_CLC.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MOVI/single-assemblies/MOVI_MIRA.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MOVI/single-assemblies/MOVI_ABySS_32.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MSCb/single-assemblies/MSCb_CLC.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MSCb/single-assemblies/MSCb_MIRA.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MSCb/single-assemblies/MSCb_ABySS_29.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MSCc/single-assemblies/MSCc_CLC.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MSCc/single-assemblies/MSCc_MIRA.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MSCc/single-assemblies/MSCc_ABySS_27.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MSCd/single-assemblies/MSCd_CLC.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MSCd/single-assemblies/MSCd_MIRA.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MSCd/single-assemblies/MSCd_ABySS_34.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MSCe/single-assemblies/MSCe_CLC.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MSCe/single-assemblies/MSCe_MIRA.fasta \
+http://services.cbib.u-bordeaux2.fr/mix/MSCe/single-assemblies/MSCe_ABySS_35.fasta 
 
 mv ./MAUR/single-assemblies/*.fasta ./MAUR/
 mv ./MBOVb/single-assemblies/*.fasta ./MBOVb/
